269de42f87a4504d978e384d1b6b119b337938f1
litelp <lponomareva97@yandex.ru>
2024-10-27 20:56:59 +0400

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let cfg = bracketsConfig;
+  let result = [];
+  let eqalsBr = 0;
+  let openBr = [];
+  let closeBr = [];
+  let countBr = [];
+  for (let n = 0; n < cfg.length;n++) {
+      openBr.push(cfg[n][0]);
+      closeBr.push(cfg[n][1]);
+      countBr[n] = 0;
+  }
+  console.log('Open = ', openBr, 'Close = ', closeBr)
+  for (let i = 0;i < str.length;i++){
+      if ( (openBr.indexOf(str[i])) == (closeBr.indexOf(str[i])) ) {
+          if (eqalsBr % 2 == 0) {
+              result.push(str[i]);
+              countBr[openBr.indexOf(str[i])] += 1;
+              eqalsBr += 1;
+          } else if (eqalsBr % 2 == 1) {
+              eqalsBr -= 1;
+              result.pop();
+              countBr[closeBr.indexOf(str[i])] -= 1;
+          }
+      } else if (openBr.indexOf(str[i]) !== -1) {
+          result.push(str[i]);
+          countBr[openBr.indexOf(str[i])] += 1;
+      } else if (closeBr.indexOf(str[i]) !== -1) {
+          countBr[closeBr.indexOf(str[i])] -= 1;
+          if (closeBr.indexOf(str[i]) == openBr.indexOf(result[result.length - 1])) {
+              result.pop();
+
+          }
+      }
+  }
+
+  countBr.forEach(element => {
+      (element !== 0) ? (eqalsBr += 1) : (eqalsBr = eqalsBr);
+  });
+
+  if (result.length === 0 && eqalsBr === 0) {
+      return true
+  } else {
+      return false;
+  }
 }
